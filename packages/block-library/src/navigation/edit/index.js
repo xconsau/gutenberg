3a20328f852f4f7feed692f3d3bc98369c203874
Andrei Draganescu <andrei.draganescu@automattic.com>
2022-09-28 22:42:25 +0300

    follow the design on where the list view is placed

--- packages/block-library/src/navigation/edit/index.js
+++ packages/block-library/src/navigation/edit/index.js
@@ -515,18 +515,6 @@ function Navigation( {
 
 	const stylingInspectorControls = (
 		<InspectorControls>
-			<PanelBody title={ __( 'Navigation structure' ) }>
-				<BlockEditorProvider
-					value={ navigationInnerBlocks }
-					onChange={ onChange }
-					onInput={ onInput }
-				>
-					<ListView
-						blocks={ navigationInnerBlocks }
-						isExpanded={ true }
-					/>
-				</BlockEditorProvider>
-			</PanelBody>
 			{ hasSubmenuIndicatorSetting && (
 				<PanelBody title={ __( 'Display' ) }>
 					{ isResponsive && (
@@ -702,6 +690,16 @@ function Navigation( {
 							/* translators: %s: The name of a menu. */
 							actionLabel={ __( "Switch to '%s'" ) }
 						/>
+						<BlockEditorProvider
+							value={ navigationInnerBlocks }
+							onChange={ onChange }
+							onInput={ onInput }
+						>
+							<ListView
+								blocks={ navigationInnerBlocks }
+								isExpanded={ true }
+							/>
+						</BlockEditorProvider>
 						<Button
 							variant="link"
 							disabled={
@@ -785,6 +783,16 @@ function Navigation( {
 							/* translators: %s: The name of a menu. */
 							actionLabel={ __( "Switch to '%s'" ) }
 						/>
+						<BlockEditorProvider
+							value={ navigationInnerBlocks }
+							onChange={ onChange }
+							onInput={ onInput }
+						>
+							<ListView
+								blocks={ navigationInnerBlocks }
+								isExpanded={ true }
+							/>
+						</BlockEditorProvider>
 						<Button
 							variant="link"
 							disabled={
@@ -903,6 +911,16 @@ function Navigation( {
 							/* translators: %s: The name of a menu. */
 							actionLabel={ __( "Switch to '%s'" ) }
 						/>
+						<BlockEditorProvider
+							value={ navigationInnerBlocks }
+							onChange={ onChange }
+							onInput={ onInput }
+						>
+							<ListView
+								blocks={ navigationInnerBlocks }
+								isExpanded={ true }
+							/>
+						</BlockEditorProvider>
 						<Button
 							variant="link"
 							disabled={
