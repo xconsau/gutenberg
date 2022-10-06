cabae08a232baf02fbb0ebd5803d90a80f9ae105
Ella van Durpe <ella@vandurpe.com>
2022-10-06 10:23:36 +0200

    Writing flow: add Shift+Arrow when selection is 'full'

--- packages/block-editor/src/components/writing-flow/use-arrow-nav.js
+++ packages/block-editor/src/components/writing-flow/use-arrow-nav.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import scrollIntoView from 'dom-scroll-into-view';
+
+/**
  * WordPress dependencies
  */
 import {
@@ -9,6 +14,7 @@ import {
 	placeCaretAtHorizontalEdge,
 	placeCaretAtVerticalEdge,
 	isRTL,
+	getScrollContainer,
 } from '@wordpress/dom';
 import { UP, DOWN, LEFT, RIGHT } from '@wordpress/keycodes';
 import { useDispatch, useSelect } from '@wordpress/data';
@@ -142,11 +148,13 @@ export default function useArrowNav() {
 	const {
 		getMultiSelectedBlocksStartClientId,
 		getMultiSelectedBlocksEndClientId,
+		getPreviousBlockClientId,
+		getNextBlockClientId,
 		getSettings,
 		hasMultiSelection,
 		__unstableIsFullySelected,
 	} = useSelect( blockEditorStore );
-	const { selectBlock } = useDispatch( blockEditorStore );
+	const { selectBlock, multiSelect } = useDispatch( blockEditorStore );
 	return useRefEffect( ( node ) => {
 		// Here a DOMRect is stored while moving the caret vertically so
 		// vertical position of the start position can be restored. This is to
@@ -200,6 +208,40 @@ export default function useArrowNav() {
 				}
 
 				if ( shiftKey ) {
+					const selectionStartClientId =
+						getMultiSelectedBlocksStartClientId();
+					const selectionEndClientId =
+						getMultiSelectedBlocksEndClientId();
+					const selectionBeforeEndClientId =
+						getPreviousBlockClientId( selectionEndClientId );
+					const selectionAfterEndClientId =
+						getNextBlockClientId( selectionEndClientId );
+					const nextSelectionEndClientId = isReverse
+						? selectionBeforeEndClientId
+						: selectionAfterEndClientId;
+
+					if ( nextSelectionEndClientId ) {
+						if (
+							selectionStartClientId === nextSelectionEndClientId
+						) {
+							selectBlock( nextSelectionEndClientId );
+						} else {
+							multiSelect(
+								selectionStartClientId,
+								nextSelectionEndClientId
+							);
+							scrollIntoView(
+								node.ownerDocument.getElementById(
+									`block-${ nextSelectionEndClientId }`
+								),
+								getScrollContainer( node ) ||
+									node.ownerDocument.defaultView,
+								{
+									onlyScrollIfNeeded: true,
+								}
+							);
+						}
+					}
 					return;
 				}
 
