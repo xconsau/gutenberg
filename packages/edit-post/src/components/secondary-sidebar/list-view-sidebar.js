093139cbad3740ecc4110ad0459bb4512f3730d3
Jorge <jorge.costa@developer.pt>
2022-10-07 21:05:55 +0100

    Move document information and outline to list view panel

--- packages/edit-post/src/components/secondary-sidebar/list-view-sidebar.js
+++ packages/edit-post/src/components/secondary-sidebar/list-view-sidebar.js
@@ -1,18 +1,32 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
-import { __experimentalListView as ListView } from '@wordpress/block-editor';
-import { Button } from '@wordpress/components';
+import {
+	__experimentalListView as ListView,
+	store as blockEditorStore,
+} from '@wordpress/block-editor';
+import { Button, __experimentalText as Text } from '@wordpress/components';
 import {
 	useFocusOnMount,
 	useFocusReturn,
-	useInstanceId,
 	useMergeRefs,
 } from '@wordpress/compose';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { closeSmall } from '@wordpress/icons';
 import { ESCAPE } from '@wordpress/keycodes';
+import { useState } from '@wordpress/element';
+import {
+	DocumentOutline,
+	WordCount,
+	TimeToRead,
+	CharacterCount,
+} from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -20,6 +34,12 @@ import { ESCAPE } from '@wordpress/keycodes';
 import { store as editPostStore } from '../../store';
 
 export default function ListViewSidebar() {
+	const { headingCount } = useSelect( ( select ) => {
+		const { getGlobalBlockCount } = select( blockEditorStore );
+		return {
+			headingCount: getGlobalBlockCount( 'core/heading' ),
+		};
+	}, [] );
 	const { setIsListViewOpened } = useDispatch( editPostStore );
 
 	const focusOnMountRef = useFocusOnMount( 'firstElement' );
@@ -32,35 +52,94 @@ export default function ListViewSidebar() {
 		}
 	}
 
-	const instanceId = useInstanceId( ListViewSidebar );
-	const labelId = `edit-post-editor__list-view-panel-label-${ instanceId }`;
+	const [ tab, setTab ] = useState( 'list-view' );
 
 	return (
 		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
 		<div
-			aria-labelledby={ labelId }
+			aria-label={ __( 'List View' ) }
 			className="edit-post-editor__list-view-panel"
 			onKeyDown={ closeOnEscape }
 		>
 			<div
-				className="edit-post-editor__list-view-panel-header"
+				className="edit-post-editor__list-view-panel-header components-panel__header edit-post-sidebar__panel-tabs"
 				ref={ headerFocusReturnRef }
 			>
-				<strong id={ labelId }>{ __( 'List View' ) }</strong>
 				<Button
 					icon={ closeSmall }
 					label={ __( 'Close List View Sidebar' ) }
 					onClick={ () => setIsListViewOpened( false ) }
 				/>
+				<ul>
+					<li>
+						<Button
+							onClick={ () => {
+								setTab( 'list-view' );
+							} }
+							className={ classnames(
+								'edit-post-sidebar__panel-tab',
+								{ 'is-active': tab === 'list-view' }
+							) }
+							aria-current={ tab === 'list-view' }
+						>
+							{ __( 'List View' ) }
+						</Button>
+					</li>
+					<li>
+						<Button
+							onClick={ () => {
+								setTab( 'outline' );
+							} }
+							className={ classnames(
+								'edit-post-sidebar__panel-tab',
+								{ 'is-active': tab === 'outline' }
+							) }
+							aria-current={ tab === 'outline' }
+						>
+							{ __( 'Outline' ) }
+						</Button>
+					</li>
+				</ul>
 			</div>
 			<div
-				className="edit-post-editor__list-view-panel-content"
 				ref={ useMergeRefs( [
 					contentFocusReturnRef,
 					focusOnMountRef,
 				] ) }
+				className="edit-post-editor__list-view-container"
 			>
-				<ListView />
+				{ tab === 'list-view' && (
+					<div className="edit-post-editor__list-view-panel-content">
+						<ListView />
+					</div>
+				) }
+				{ tab === 'outline' && (
+					<>
+						{ headingCount > 0 ? (
+							<DocumentOutline />
+						) : (
+							<p className="edit-post-editor__list-view-empty-headings">
+								{ __( 'There are no headings on this post.' ) }
+							</p>
+						) }
+						<div className="edit-post-editor__list-view-overview">
+							<div>
+								<Text>{ __( 'Characters:' ) }</Text>
+								<Text>
+									<CharacterCount />
+								</Text>
+							</div>
+							<div>
+								<Text>{ __( 'Words:' ) }</Text>
+								<WordCount />
+							</div>
+							<div>
+								<Text>{ __( 'Time to read:' ) }</Text>
+								<TimeToRead />
+							</div>
+						</div>
+					</>
+				) }
 			</div>
 		</div>
 	);
